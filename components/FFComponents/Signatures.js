f93065fbf79b03d8a1c451266611a32c54d7bbea
Nathan Elkind <nathanaelkind@gmail.com>
2021-11-30 13:45:18 -0600

    Clean slate
    Took out signature code. Fully functional state.

--- components/FFComponents/Signatures.js
+++ components/FFComponents/Signatures.js
@@ -5,41 +5,10 @@ import SignatureCapture from 'react-native-signature-capture'
 const Signatures = ({hasspouse,
             }) => {
 
-    const sign = createRef();
-
-    const saveSign = () => {
-        sign.current.saveImage();
-    };
-    
-    const resetSign = () => {
-        sign.current.resetImage();
-    };
-    
-    const _onSaveEvent = (result) => {
-        //result.encoded - for the base64 encoded png
-        //result.pathName - for the file path name
-        alert('Signature Captured Successfully');
-        console.log(result.encoded);
-    };
-    
-    const _onDragEvent = () => {
-        // This callback will be called when the user enters signature
-        console.log('dragged');
-    };
-
     return (
         <View style={styles.fullWidth, {flex: 1}}>
             <Text style={styles.titleText}>Signatures</Text>
             <Text style={styles.headerText}>Client Signature:</Text>
-            <SignatureCapture
-                style={styles.signature}
-                ref={sign}
-                onSaveEvent={_onSaveEvent}
-                onDragEvent={_onDragEvent}
-                showNativeButtons={false}
-                showTitleLabel={false}
-                viewMode={'portrait'}
-            />
             { hasspouse ? <>
             <Text style={styles.headerText}>Spouse Signature:</Text>
 
